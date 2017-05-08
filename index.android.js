7032be7e029a267e5447ea3b37e300989464053a
Ahamad <ahamad.jedu@gmail.com>
2017-05-09 03:15:03 +0700

    23. Styling with React Native

--- index.android.js
+++ index.android.js
@@ -13,20 +13,7 @@ import {
 import Header from './src/components/header';
 
 const App = () => {
-
-    return  <View >
-       <Header/>
-        <Text >
-          Welcome to React Native!
-        </Text>
-        <Text >
-          To get started, edit index.android.js
-        </Text>
-        <Text >
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>;
+    return <Header/>;
 };
 
 
--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,15 @@ import React from 'react';
 import {Text} from 'react-native';
 
 const Header = () => {
-  return <Text>Albums!</Text>;
+  const {textStyle} = Styles;
+
+  return <Text style={textStyle}>Albums!</Text>;
 };
+
+const Styles = {
+  textStyle:{
+    fontSize:25,
+  }
+}
 
 export default Header;
