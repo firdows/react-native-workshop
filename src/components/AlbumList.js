1e6b27353901121659a612a7615f4c73636a2eba
Ahamad <ahamad.jedu@gmail.com>
2017-05-09 09:15:04 +0700

    29. List Component Boilerplate

---
+++ src/components/AlbumList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactNative,{View,Text} from 'react-native';
+
+
+const AlbumList = () => {
+  return (
+    <View>
+      <Text>AlbumList !!!!</Text>
+    </View>
+  );
+}
+
+
+
+
+export default AlbumList;
