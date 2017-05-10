883466a0322217fd8f60f64924a2458690dd8231
Ahamad <ahamad.jedu@gmail.com>
2017-05-10 07:42:40 +0700

    41. Dividing Cards into Sections

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,19 +2,33 @@ import React from 'react';
 import {View,Text,Image} from 'react-native';
 
 import Card from './Card';
+import CardSection from './CardSection';
 
 
 
 const AlbumDetail = (props) => {
   return (
     <Card>
+      <CardSection>
+        <Image source={props.album.thumbnail_image} />
+        <Text>{props.album.title}</Text>
+        <Text>{props.album.artist}</Text>
+      </CardSection>
 
-      <Text>{props.album.title}</Text>
-      <Text>{props.album.artist}</Text>
-
+      <CardSection>
+        <Image style={styles.imageStyle} source={props.album.thumbnail_image} />
+      </CardSection>
     </Card>
   );
 };
 
+const styles = {
+  imageStyle:{
+    resizeMode: 'cover',
+    width: 100,
+    height: 200,
+    flex: 1
+  }
+};
 
 export default AlbumDetail;
