ecd8bb442c45063502cce3c7cfcce7f02263994b
Jehdu Ahamad <ahamad.jedu@gmail.com>
2017-05-12 10:41:54 +0700

    58. Firebase Client Setup

--- src/app.js
+++ src/app.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-
-
+import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 import AlbumList from './components/album/AlbumList';
 
+
 class App extends Component {
+
+  componentWillMount() {
+    firebase.initializaApp({
+      apiKey: 'AIzaSyA5TUh-vCv4b33gzTnhZ60HDL7pX_VG_CM',
+      authDomain: 'authentication-e132b.firebaseapp.com',
+      databaseURL: 'https://authentication-e132b.firebaseio.com',
+      projectId: 'authentication-e132b',
+      storageBucket: 'authentication-e132b.appspot.com',
+      messagingSenderId: '443475363193'
+    });
+  }
 
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <Header headerText={'อัลบั้ม'} />
-        <AlbumList />
+        <Header headerText={'Authentication'} />
+        {/*<AlbumList />*/}
+        <Text>{'Authen'}</Text>
       </View>
     );
   }
