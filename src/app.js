5c89c5c0ae2913419fb755e3fb6380fd903e4c8f
Jehdu Ahamad <ahamad.jedu@gmail.com>
2017-05-12 19:20:13 +0700

    59. Login Form Scaffolding

--- src/app.js
+++ src/app.js
@@ -3,19 +3,19 @@ import { View, Text } from 'react-native';
 import firebase from 'firebase';
 import { Header } from './components/common';
 import AlbumList from './components/album/AlbumList';
+import LoginForm from './components/auth/LoginForm';
 
 
 class App extends Component {
-
   componentWillMount() {
-    firebase.initializaApp({
-      apiKey: 'AIzaSyA5TUh-vCv4b33gzTnhZ60HDL7pX_VG_CM',
-      authDomain: 'authentication-e132b.firebaseapp.com',
-      databaseURL: 'https://authentication-e132b.firebaseio.com',
-      projectId: 'authentication-e132b',
-      storageBucket: 'authentication-e132b.appspot.com',
-      messagingSenderId: '443475363193'
-    });
+    // firebase.initializaApp({
+    //   apiKey: 'AIzaSyA5TUh-vCv4b33gzTnhZ60HDL7pX_VG_CM',
+    //   authDomain: 'authentication-e132b.firebaseapp.com',
+    //   databaseURL: 'https://authentication-e132b.firebaseio.com',
+    //   projectId: 'authentication-e132b',
+    //   storageBucket: 'authentication-e132b.appspot.com',
+    //   messagingSenderId: '443475363193'
+    // });
   }
 
   render() {
@@ -23,7 +23,7 @@ class App extends Component {
       <View style={{ flex: 1 }}>
         <Header headerText={'Authentication'} />
         {/*<AlbumList />*/}
-        <Text>{'Authen'}</Text>
+        <LoginForm />
       </View>
     );
   }
