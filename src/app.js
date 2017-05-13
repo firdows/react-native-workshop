1a9e8eb32bd152042ecffe95ba36cc43e5d7514d
Jehdu Ahamad <ahamad.jedu@gmail.com>
2017-05-13 10:22:27 +0700

    78. The Basics of Redux & 79. More on Redux

--- src/app.js
+++ src/app.js
@@ -71,3 +71,28 @@ class App extends Component {
 }
 
 export default App;
+
+
+/*
+# Exsample Redux
+
+const reducer = (state = [], action) => {
+  if (action.type === 'split_string') {
+    return action.payload.split('');
+  }
+  return state;
+};
+const store = Redux.createStore(reducer);
+
+store.getState();
+
+const action = { 
+  type: 'split_string',
+  payload: 'asdf'
+};
+
+store.dispatch(action);
+
+store.getState();
+
+*/
