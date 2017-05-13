fe38ecf3af7a854759ec78281ef1614250cb5f10
Jehdu Ahamad <ahamad.jedu@gmail.com>
2017-05-13 10:37:02 +0700

    80. Redux is Hard

--- src/app.js
+++ src/app.js
@@ -76,23 +76,37 @@ export default App;
 /*
 # Exsample Redux
 
+//เซ็ตการทำงานของ reducer
 const reducer = (state = [], action) => {
   if (action.type === 'split_string') {
     return action.payload.split('');
+  } else if (action.type === 'add_char') {//เพิ่ม
+    //state.push(action.payload);
+    //return state;
+    return [ ...state, action.payload ];
   }
+  
   return state;
 };
-const store = Redux.createStore(reducer);
 
+//สร้าง store
+const store = Redux.createStore(reducer);
 store.getState();
 
+//เช็ตการกระทำตาม type และส่งค่าลงไปใน payload
 const action = { 
   type: 'split_string',
   payload: 'asdf'
 };
-
 store.dispatch(action);
+store.getState();
 
+//เซ็ตการทำใหม่
+const action2 = { 
+  type: 'add_char',
+  payload: 'qwer'
+};
+store.dispatch(action2);
 store.getState();
 
 */
